5f0f8db3bcc5c3f9bea6e83efc4d613f922d5ebc
HwangHyeJin <h960502hhj@gmail.com>
2022-03-05 17:42:48 +0900

    feat:220305 변수 study

---
+++ 220305.js
@@ -0,0 +1,19 @@
+let age = 25;
+
+let tall = 169;
+
+let inf = Infinity;
+
+let minusInf = -Infinity;
+
+let name = "winterlood";
+
+let name2 = "황혜진";
+
+let name3 = "백틱";
+
+let numberA = 12;
+
+let numberB = "2";
+
+console.log(numberA * numberB);
